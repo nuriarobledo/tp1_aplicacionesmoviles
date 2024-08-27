baf7b7e97502832620751ae760b639b37eb97e3c
GonzaloEBaez <gezbaez@gmail.com>
2024-08-27 17:43:56 -0300

    Lógica del login

--- app/src/main/java/com/example/tp1_aplicacionesmoviles/login.kt
+++ app/src/main/java/com/example/tp1_aplicacionesmoviles/login.kt
@@ -1,12 +1,20 @@
 package com.example.tp1_aplicacionesmoviles
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent //permite navegar entre pantallas
+import android.os.Bundle //Contiene los datos de la instancia anterior de la actividad si es que se está recreando.
+import android.util.Log //Se utiliza para escribir mensajes de depuración.
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge //Método para habilitar el diseño de borde a borde.
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
+import androidx.core.view.ViewCompat //Herramientas de compatibilidad para manejar las ventanas e insets en Android.
 import androidx.core.view.WindowInsetsCompat
 
-class login : AppCompatActivity() {
+class Login : AppCompatActivity() {
+
+    private val tag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,40 @@ class login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnIniciarSesion = findViewById<Button>(R.id.btnIniciarSesion)
+        btnIniciarSesion.setOnClickListener{
+
+            val etUsuario = findViewById<EditText>(R.id.etUsuario).text.toString()
+
+            val etContrasena = findViewById<EditText>(R.id.etContrasena).text.toString()
+
+            // Agregar logs para depuración
+
+            Log.d(tag, "Usuario ingresado: $etUsuario")
+            Log.d(tag, "Contraseña ingresada: $etContrasena")
+
+            if (etUsuario == "Juan Torres" && etContrasena == "1234utn"){
+                Log.i(tag, "Login correcto")
+                val intent = Intent(this, "WelcomeActivity"::class.java).apply {
+                    putExtra("USERNAME", etUsuario)
+                }
+                startActivity(intent)
+                Toast.makeText(this, "Login correcto", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.e(tag, "Usuario o contraseña incorrectos")
+
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val btnRegistrate = findViewById<Button>(R.id.btnRegistrate)
+        btnRegistrate.setOnClickListener{
+            Log.d(tag, "Botón de registro presionado")
+
+            val intent = Intent(this, "RegisterActivity"::class.java)
+            startActivity(intent)
         }
     }
 }
